a176d0fe5e895bfd4265d6b7c327c463016bb6e6
周韧平 <2021010699@secoder.net>
2023-03-22 11:41:44 +0800

    deploy test

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -6,43 +6,43 @@ import { stringToBoard } from "../utils/logic";
 import { request } from "../utils/network";
 import { Board } from "../utils/types";
 
-const BoardScreenWithInit = () => {
-    const [initBoard, setInitBoard] = useState<Board | undefined>(undefined);
-    const [initBoardName, setInitBoardName] = useState<string>("");
-    const [initUserName, setInitUserName] = useState<string>("");
-    const [refreshing, setRefreshing] = useState(true);
+// const BoardScreenWithInit = () => {
+//     const [initBoard, setInitBoard] = useState<Board | undefined>(undefined);
+//     const [initBoardName, setInitBoardName] = useState<string>("");
+//     const [initUserName, setInitUserName] = useState<string>("");
+//     const [refreshing, setRefreshing] = useState(true);
     
-    const router = useRouter();
-    const query = router.query;
+//     const router = useRouter();
+//     const query = router.query;
 
-    useEffect(() => {
-        if (!router.isReady) {
-            return;
-        }
+//     useEffect(() => {
+//         if (!router.isReady) {
+//             return;
+//         }
 
-        request(`/api/boards/${router.query.id}`, "GET")
-            .then((res) => {
-                setInitBoard(stringToBoard(res.board));
-                setInitBoardName(res.boardName);
-                setInitUserName(res.userName);
-                setRefreshing(false);
-            })
-            .catch((err) => {
-                alert(FAILURE_PREFIX + err);
-                router.push("/");
-            });
-    }, [router, query]);
+//         request(`/api/boards/${router.query.id}`, "GET")
+//             .then((res) => {
+//                 setInitBoard(stringToBoard(res.board));
+//                 setInitBoardName(res.boardName);
+//                 setInitUserName(res.userName);
+//                 setRefreshing(false);
+//             })
+//             .catch((err) => {
+//                 alert(FAILURE_PREFIX + err);
+//                 router.push("/");
+//             });
+//     }, [router, query]);
     
-    return refreshing ? (
-        <p> Loading... </p>
-    ) : (
-        <BoardScreen init={{
-            id: Number(router.query.id),
-            initBoard: initBoard as Board,
-            initBoardName,
-            initUserName,
-        }} />
-    );
-};
+//     return refreshing ? (
+//         <p> Loading... </p>
+//     ) : (
+//         <BoardScreen init={{
+//             id: Number(router.query.id),
+//             initBoard: initBoard as Board,
+//             initBoardName,
+//             initUserName,
+//         }} />
+//     );
+// };
 
-export default BoardScreenWithInit;
+// export default BoardScreenWithInit;
