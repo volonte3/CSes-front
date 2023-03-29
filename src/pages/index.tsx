a3d0f3ca871f90764a61cabe993e88b901f379e5
苏禹畅 <suyc21@mails.tsinghua.edu.cn>
2023-03-30 00:12:29 +0800

    [002]尝试解决路由跳转的问题#2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,14 @@
-// export default Login;
 import LoginUI from "../components/LoginUI";
-// import { withRouter, Prompt } from "react-router-dom"; // 从react-router-dom中引出Prompt组件
-// import { useCookies } from "react-cookies";
-// import cookie from "react-cookies";
 import {Button,Form} from "antd";
-// import { request } from "../../utils/network";
-
+import { useRouter } from "next/router";
 
 const Login = () => {
-    // LoginControl();
-    // const state =  { token: loginUser() }
-    // console.log(state)
-    // loginUser();
-    return <LoginUI />;
-    //  <div style={{ display: "flex", justifyContent: "center" }}>
-    {/* <Form.Item > */}
-
-    {/* <Button type="primary" htmlType="submit" className="Test" onClick={()=>{onLogin("aaa");window.location.reload();}}>
-            s
-        </Button> */}
-        
-    {/* </Form.Item> */}
-    // </div>;
+    const router = useRouter();
+    const isRoot = router.pathname === "/";
+    return !isRoot ? (
+        <p>mistake</p>
+    )
+        :(<LoginUI />);
 };
 
 export default Login;
