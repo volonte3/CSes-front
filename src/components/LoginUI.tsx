6665ccb611771c2c6e8dd44cffdc60991be7c76f
周韧平 <2021010699@secoder.net>
2023-03-30 03:22:40 +0000

    [002]将dev上debug好的页面放到master分支上部署

--- src/components/LoginUI.tsx
+++ src/components/LoginUI.tsx
@@ -1,12 +1,54 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Checkbox, Image } from "antd";
+import { Form, Input, Button, Checkbox, Image, Modal } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import BackGround from "public/LoginBackground.png";
+import { FAILURE_PREFIX } from "../constants/string";
 import { useRouter } from "next/router";
+import cookie from "react-cookies";
+import { request } from "../utils/network";
+import CryptoJS from "crypto-js";
+import {
+    generateRandomString,
+    getCookie,
+    setCookie,
+} from "../utils/CookieOperation";
 
-const LoginUI = () => {
+export const loginUser = () => {
+    return cookie.load("userInfo");
+};
+interface LoginInit {
+    initUserName: string,
+    initPassword: string,
+}
+export interface LoginScreenProps {
+    init?: LoginInit,
+}
+
+
+const LoginUI = (props: LoginScreenProps) => {
+    const [UserName, setUserName] = useState<string>(props.init?.initUserName ?? "");
+    const [Password, setPassword] = useState<string>(props.init?.initPassword ?? "");
     const [loading, setLoading] = useState(false);
     const router = useRouter();
+    const loginUser = () => {
+        return cookie.load("userInfo");
+    };
+    const loginSendMessage = () => {
+        // Step 6 BEGIN
+
+        request(
+            "/api/User/login",
+            "POST",
+            {
+                UserName: UserName,
+                Password: CryptoJS.MD5(Password).toString(),
+            }
+        )
+            .then(() => router.push("/main_page"))
+            // .catch((err) => alert(FAILURE_PREFIX + err));
+            .catch((err) => ErrorInfo(err));
+        // Step 6 END
+    };
     const onFinish = (values: any) => {
         setLoading(true);
         setTimeout(() => {
@@ -14,7 +56,25 @@ const LoginUI = () => {
             console.log("Received values of form: ", values);
         }, 2000);
     };
+    // 用户登录，保存cookie
+    const onLogin = () => {
+        let SessionID = generateRandomString(32);
+        console.log("SessionID is",SessionID);
+        cookie.save("SessionID", SessionID, { path: "/" });
+    };
 
+    const ErrorInfo = (errinfo: string) => {
+        Modal.error({
+            title: "登录失败",
+            content: errinfo.toString().substring(5),
+        });
+    };
+
+    // 用户登出，删除cookie
+    const logout = () => {
+        cookie.remove("userInfo");
+        // window.location.href = "/";
+    };
     return (
         <div style={{
             display: "flex", justifyContent: "center", alignItems: "center", height: "100vh",
@@ -35,7 +95,9 @@ const LoginUI = () => {
                             name="username"
                             rules={[{ required: true, message: "用户名不能为空!" }]}
                         >
-                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username"
+                                onChange={(e) => setUserName(e.target.value)}
+                            />
                         </Form.Item>
 
                         <Form.Item
@@ -46,6 +108,7 @@ const LoginUI = () => {
                                 prefix={<LockOutlined className="site-form-item-icon" />}
                                 type="password"
                                 placeholder="Password"
+                                onChange={(e) => setPassword(e.target.value)}
                             />
                         </Form.Item>
                         {/* <img
@@ -62,14 +125,14 @@ const LoginUI = () => {
                             </a> */}
                         </Form.Item>
 
-                        {/* <Form.Item >
+                        <Form.Item >
                             <div style={{ display: "flex", justifyContent: "center" }}>
-                                <Button type="disabled" htmlType="submit" className="login-form-button" loading={loading} >
+                                <Button type="primary" htmlType="submit" className="login-form-button" loading={loading} 
+                                    onClick={() => {onLogin();loginSendMessage();}}>
                                     登录
-                                </Button> */}
-                        {/* Or <a href="/">register now!</a> */}
-                        {/* </div>
-                        </Form.Item> */}
+                                </Button>
+                            </div>
+                        </Form.Item>
 
                     </Form>
                 </div>
