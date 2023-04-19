159273cf85303dd01886217369b666bace365775
周韧平 <2021010699@secoder.net>
2023-04-20 00:42:56 +0800

    [006]添加了四个操作的api请求#6

--- src/components/AssetList.tsx
+++ src/components/AssetList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { theme, Space, Table, Button } from "antd";
+import { theme, Space, Table, Button, Modal, Menu } from "antd";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { request } from "../utils/network";
@@ -12,14 +12,9 @@ interface AssetListProps {
     Assets: AssetData[]
 }
 const AssetList = () => {
-    const [data, setData] = useState<AssetData[]>(); // 存储加载该系统管理员管理的资产管理员和员工的信息
-    const [searchText, setSearchText] = useState<string>(""); // 存储搜索框中输入的值
-    const [IsSomeRowReceiveFalse,setIsSomeRowReceiveFalse] = useState<boolean>(true);
-    const [IsSomeRowTransfersFalse,setIsSomeRowTransfersFalse] = useState<boolean>(true);
-    const onSearch = (value: string) => {
-        setSearchText(value);
-    };
-    
+    const [IsSomeRowReceiveFalse, setIsSomeRowReceiveFalse] = useState<boolean>(true);
+    const [IsSomeRowTransfersFalse, setIsSomeRowTransfersFalse] = useState<boolean>(true);
+    const [SelectedRows, setSelectedRows] = useState<AssetData[]>([]);
     const columns: ProColumns<AssetData>[] = [
         {
             title: "资产编号",
@@ -86,11 +81,16 @@ const AssetList = () => {
             render: (text, record, _, action) => {
                 const { IsReceive, IsReturn, IsMaintenance, IsTransfers } = record;
                 const options = [
-                    { key: "receive", name: "领用", disabled: !IsReceive },
-                    { key: "return", name: "退库", disabled: !IsReturn },
-                    { key: "maintenance", name: "维保", disabled: !IsMaintenance },
-                    { key: "transfers", name: "转移", disabled: !IsTransfers },
+                    { key: "receive", name: "领用", disabled: !IsReceive,onClick: ()=>hanleChange([record.ID], 0)},
+                    { key: "return", name: "退库", disabled: !IsReturn,onClick: ()=>hanleChange([record.ID], 1)},
+                    { key: "maintenance", name: "维保", disabled: !IsMaintenance, onClick: ()=>hanleChange([record.ID], 2) },
+                    { key: "transfers", name: "转移", disabled: !IsTransfers},
                 ];
+                const menuItems = options.map(option => (
+                    <Menu.Item key={option.key} disabled={option.disabled} onClick={option.onClick}>
+                        {option.name}
+                    </Menu.Item>
+                ));
                 return (
                     <TableDropdown
                         key="actionGroup"
@@ -101,34 +101,39 @@ const AssetList = () => {
             },
         }
     ];
-    // const AssetDataSource: AssetData[] = [];
-    // for (let i = 0; i < 20; i += 1) {
-    //     tableListDataSource.push({
-    //       key: i,
-    //       name: 'AppName-' + i,
-    //       containers: Math.floor(Math.random() * 20),
-    //       callNumber: Math.floor(Math.random() * 2000),
-    //       progress: Math.ceil(Math.random() * 100) + 1,
-    //       creator: creators[Math.floor(Math.random() * creators.length)],
-    //       status: valueEnum[Math.floor(Math.random() * 10) % 4],
-    //       createdAt: Date.now() - Math.floor(Math.random() * 100000),
-    //       memo: i % 2 === 1 ? '很长很长很长很长很长很长很长的文字要展示但是要留下尾巴' : '简短备注文案',
-    //     });
-    //   }
-    const FetchAssetList = () => {
-        request(`/api/User/member/${LoadSessionID()}`, "GET")
-            .then((res) => {
-                setData(res.Asset);
-            });
-    };
     const router = useRouter();
     const query = router.query;
     useEffect(() => {
         if (!router.isReady) {
             return;
         }
-        // FetchAssetList();
     }, [router, query]);
+    const hanleChange = (AssetIDList: string[], operation: number, MoveTo: string = "") => {
+        request(`api/Asset/Apply/${LoadSessionID()}`, "POST",
+            {
+                "operation": operation,
+                "AssetList": AssetIDList,
+                "MoveTo": MoveTo,
+            }
+        )
+            .then(() => {
+
+                Modal.success({
+                    title: "申请成功",
+                    content: "成功提交请求",
+                });
+            })
+            .catch(
+                (err: string) => {
+                    if (IfCodeSessionWrong(err, router)) {
+                        Modal.error({
+                            title: "申请失败",
+                            content: err.toString().substring(5),
+                        });
+                    }
+                }
+            );
+    };
     const themeConfig = {
         token: {
             colorPrimary: "red",
@@ -143,18 +148,6 @@ const AssetList = () => {
         <ProTable
             columns={columns}
             options={false}
-            // cardProps={{ title: "业务定制", bordered: true }}
-            // request={(params) => {
-            //     const filteredData = props.Assets.filter(
-            //         (item) =>
-            //             item.Description.includes(searchText)
-            //     );
-
-            //     return Promise.resolve({
-            //         data: filteredData,
-            //         success: true,
-            //     });
-            // }}
             rowKey="ID"
             rowSelection={{
                 // 自定义选择项参考: https://ant.design/components/table-cn/#components-table-demo-row-selection-custom
@@ -163,14 +156,14 @@ const AssetList = () => {
                 defaultSelectedRowKeys: [],
             }}
             tableAlertRender={({ selectedRowKeys, selectedRows, onCleanSelected }) => {
-                // const ans = selectedRows.some(row => {console.log("zzzz",row);});
                 setIsSomeRowReceiveFalse(selectedRows.some(row => !row.IsReceive));
                 setIsSomeRowTransfersFalse(selectedRows.some(row => !row.IsTransfers));
+                setSelectedRows(selectedRows);
                 console.log(selectedRowKeys, selectedRows);
                 return (
                     <Space size={4}>
                         已选 {selectedRowKeys.length} 项
-                        <a style={{ marginInlineStart: 8,color:"#007AFF"}} onClick={onCleanSelected} >
+                        <a style={{ marginInlineStart: 8, color: "#007AFF" }} onClick={onCleanSelected} >
                             取消选择
                         </a>
                     </Space>
@@ -179,7 +172,7 @@ const AssetList = () => {
             tableAlertOptionRender={() => {
                 return (
                     <Space size={16} >
-                        <Button type="primary" disabled={IsSomeRowReceiveFalse}>领用资产</Button>
+                        <Button type="primary" disabled={IsSomeRowReceiveFalse} onClick={() => hanleChange(SelectedRows.map((row: any) => row.ID), 0)}>领用资产</Button>
                         <Button type="primary" disabled={IsSomeRowTransfersFalse}>转移资产</Button>
                     </Space>
                 );
@@ -216,14 +209,6 @@ const AssetList = () => {
                                 (item: AssetData) => item.Status == params.Status
                             );
                         }
-                        // console.log(params.Description);
-                        // console.log(response.Asset);
-                        console.log(params.Status);
-                        console.log(filteredData);
-                        setData(filteredData);
-                        console.log(data);
-                        // response.Asset = data;
-                        console.log(response);
                         return Promise.resolve({ data: filteredData, success: true });
                     })
             }
@@ -239,30 +224,15 @@ const AssetList = () => {
                     return values;
                 },
             }}
-            // rowKey="id"
             scroll={{ x: "100%", y: "calc(100vh - 300px)" }}
             pagination={{
                 showSizeChanger: true
             }}
-            // search={{
-            //     searchText,
-            //     filterType: "query",
-            // }}
             search={{
                 defaultCollapsed: false,
                 defaultColsNumber: 1,
                 split: true,
                 span: 8,
-                // collapsed: false,   // 不收起查询
-                // collapseRender: () => { return <></>; },  // 收起按钮渲染为空(去掉收起按钮)
-                // optionRender: ({ searchText, resetText }, { form }) => {
-                //     return [
-                //         <Button key="searchText" type="primary"
-                //             onClick={() => { form?.submit();console.log(form); }} >{searchText}</Button>,
-                //         <Button key="resetText" type="default"
-                //             onClick={() => { form?.resetFields(); }} >{resetText}</Button>,
-                //     ];
-                // },
                 searchText: "查询"
             }}
 
