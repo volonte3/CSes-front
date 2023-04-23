792a288ea5fe2e2cc943a1a209244a96d031b5e6
张航宇 <2021010691@secoder.net>
2023-04-23 16:28:40 +0800

    [010]基本实现资产变更页面#10

---
+++ src/pages/user/asset_manager/asset_change/index.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { Breadcrumb, Layout, Menu, theme, Space, Table, Tag, Switch, Modal, Button } from "antd";
+const { Column, ColumnGroup } = Table;
+import { useRouter } from "next/router";
+const { Header, Content, Footer, Sider } = Layout;
+import { useState, useEffect } from "react";
+import { request } from "../../../../utils/network";
+import { logout, LoadSessionID } from "../../../../utils/CookieOperation";
+import AssetChange from "../../../../components/AssetChangeUI";
+import UserInfo from "../../../../components/UserInfoUI";
+import { AssetData } from "../../../../utils/types";
+import SiderMenu from "../../../../components/SiderUI";
+const App = () => {
+    const [state, setState] = useState(true); // 用户是否处在登录状态
+    const [collapsed, setCollapsed] = useState(false);
+    const [UserName, setUserName] = useState<string>(""); // 用户名
+    const [UserAuthority, setUserAuthority] = useState(2); // 用户的角色权限，0超级，1系统，2资产，3员工
+    const [UserApp, setUserApp] = useState<string>(""); // 用户显示的卡片，01串
+    const [Asset, setAsset] = useState<AssetData[]>(); // 存储加载该系统管理员管理的资产管理员和员工的信息
+    const router = useRouter();
+    const query = router.query;
+    const [Entity, setEntity] = useState<string>(""); // 实体名称
+    const [Department, setDepartment] = useState<string>("");  //用户所属部门，没有则为null
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
+    useEffect(() => {
+        if (!router.isReady) {
+            return;
+        }
+        request(
+            `/api/User/info/${LoadSessionID()}`,
+            "GET"
+        )
+            .then((res) => {
+                setState(true);
+                setUserName(res.UserName);
+                setUserApp(res.UserApp);
+                setUserAuthority(res.Authority);
+                if(res.Authority != 2 ){
+                    Modal.error({
+                        title: "无权访问",
+                        content: "请重新登录",
+                        onOk: () => { window.location.href = "/"; }
+                    });
+                }
+                setEntity(res.Entity);
+                setDepartment(res.Department);
+            })
+            .catch((err) => {
+                console.log(err.message);
+                setState(false);
+                Modal.error({
+                    title: "登录失败",
+                    content: "请重新登录",
+                    onOk: () => { window.location.href = "/"; }
+                });
+            });
+    }, [router, query, state]);
+    if (state) {
+        
+        return (
+            <Layout style={{ minHeight: "100vh" }}>
+                <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                    <div style={{ height: 32, margin: 16, background: "rgba(255, 255, 255, 0.2)" }} />
+                    <SiderMenu UserAuthority={UserAuthority} />
+                </Sider>
+                <Layout className="site-layout" >
+                    <UserInfo Name={UserName} Authority={UserAuthority} Entity={Entity} Department={Department}></UserInfo>
+                    {/* <div>
+                        <Button
+                            type="primary"
+                            // icon={<PlusSquareOutlined />}
+                            style={{ float: "left", margin: 10 }}
+                        // onClick={showDrawer1}
+                        >
+                            添加部门
+                        </Button>
+                    </div> */}
+                    <Content style={{ margin: "0 16px" }}>
+                        <Breadcrumb style={{ margin: "16px 0" }}>
+                            <Breadcrumb.Item>资产变更</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
+                            <AssetChange />
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: "center" }}>EAMS ©2023 Designed by CSes</Footer>
+                </Layout>
+            </Layout>
+        );
+    }
+};
+
+export default App;
