53fc767e114893658a413cb5e3ad0cfe9dac576e
周韧平 <2021010699@secoder.net>
2023-05-11 16:16:34 +0800

    [012]上线测试移动端显示功能#12

--- src/pages/assets/index.tsx
+++ src/pages/assets/index.tsx
@@ -1,4 +1,44 @@
-const Blank = ()=>{
-    return (<div></div>);
+import { useRouter } from "next/router";
+import { BrowserRouter as Router, Route, useLocation } from "react-router-dom";
+import { Collapse, Grid, List } from "antd-mobile";
+import { ProCard } from "@ant-design/pro-components";
+const AssetPage = () => {
+    const router = useRouter();
+    // const location = useLocation();
+    // const queryParams = new URLSearchParams(location.search);
+    // const id = queryParams.get("id");
+    const id = router.query.id;
+    const assetData = {
+        name: "Asset Name",
+        type: "Asset Type",
+        value: "$1,000",
+        description: "Asset Description",
+    };
+
+    return (
+        <div>
+            <Collapse defaultActiveKey={["1"]}>
+                <Collapse.Panel key='1' title='第一项'>
+                    <ProCard split="horizontal">
+                        <ProCard split="vertical">
+                            <ProCard title="资产名称">{assetData.name}</ProCard>
+                            <ProCard title="ID">{id}</ProCard>
+                            <ProCard title="创建时间">{assetData.name}</ProCard>
+                        </ProCard>
+                        <ProCard split="vertical">
+                            <ProCard title="当前所有者">{assetData.name}</ProCard>
+                            <ProCard title="状态">
+                                assetData.value
+                            </ProCard>
+                        </ProCard>
+                        <ProCard split="vertical">
+                            <ProCard title="资产描述">{assetData.description}</ProCard>
+                        </ProCard>
+                    </ProCard>
+                </Collapse.Panel>
+            </Collapse>
+        </div>
+    );
 };
-export default Blank;
+
+export default AssetPage;
