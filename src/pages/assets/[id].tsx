3b112a2b398c9d377db7bda12364d90f9ac9c9ac
周韧平 <2021010699@secoder.net>
2023-05-11 13:49:39 +0800

    [014]尝试去掉antd-mobile后能否正确渲染#14

--- src/pages/assets/[id].tsx
+++ src/pages/assets/[id].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { Collapse, Grid, List } from "antd-mobile";
+// import { Collapse, Grid, List } from "antd-mobile";
 import { ProCard } from "@ant-design/pro-components";
 const AssetPage = () => {
     const router = useRouter();
@@ -13,26 +13,26 @@ const AssetPage = () => {
 
     return (
         <div>
-            <Collapse defaultActiveKey={["1"]}>
-                <Collapse.Panel key='1' title='第一项'>
-                    <ProCard split="horizontal">
-                        <ProCard split="vertical">
-                            <ProCard title="资产名称">{assetData.name}</ProCard>
-                            <ProCard title="ID">{assetData.name}</ProCard>
-                            <ProCard title="创建时间">{assetData.name}</ProCard>
-                        </ProCard>
-                        <ProCard split="vertical">
-                            <ProCard title="当前所有者">{assetData.name}</ProCard>
-                            <ProCard title="状态">
-                                assetData.value
-                            </ProCard>
-                        </ProCard>
-                        <ProCard split="vertical">
-                            <ProCard title="资产描述">{assetData.description}</ProCard>
-                        </ProCard>
+            {/* <Collapse defaultActiveKey={["1"]}>
+                <Collapse.Panel key='1' title='第一项'> */}
+            <ProCard split="horizontal">
+                <ProCard split="vertical">
+                    <ProCard title="资产名称">{assetData.name}</ProCard>
+                    <ProCard title="ID">{assetData.name}</ProCard>
+                    <ProCard title="创建时间">{assetData.name}</ProCard>
+                </ProCard>
+                <ProCard split="vertical">
+                    <ProCard title="当前所有者">{assetData.name}</ProCard>
+                    <ProCard title="状态">
+                        assetData.value
                     </ProCard>
-                </Collapse.Panel>
-            </Collapse>
+                </ProCard>
+                <ProCard split="vertical">
+                    <ProCard title="资产描述">{assetData.description}</ProCard>
+                </ProCard>
+            </ProCard>
+            {/* </Collapse.Panel>
+            </Collapse> */}
         </div>
     );
 };
