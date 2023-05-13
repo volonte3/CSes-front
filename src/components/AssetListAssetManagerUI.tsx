38fec4f330373a63f37e25473907f6aedeb33938
周韧平 <2021010699@secoder.net>
2023-05-13 18:53:29 +0800

    [015]格式问题#15

--- src/components/AssetListAssetManagerUI.tsx
+++ src/components/AssetListAssetManagerUI.tsx
@@ -10,7 +10,7 @@ import { DateTransform, renderStatus, renderStatusChanges, renderStatusBadge, re
 import { DownloadOutlined } from "@ant-design/icons";
 import LabelDef from "./AssetLabelUI";
 import OSS from "ali-oss";
-import ReactHtmlParser from 'react-html-parser';
+import ReactHtmlParser from "react-html-parser";
 interface AssetListProps {
     ManagerName: string;
 }
@@ -300,9 +300,9 @@ const AssetList = (props: AssetListProps) => {
                                             </ProCard>
                                         </ProCard>
                                         <ProCard split="vertical">
-                                                <ProCard title="资产描述">
-                                                    {ReactHtmlParser(DetailInfo?.Description)}
-                                                </ProCard>
+                                            <ProCard title="资产描述">
+                                                {ReactHtmlParser(DetailInfo?.Description)}
+                                            </ProCard>
                                         </ProCard>
                                     </ProCard>
                                 </ProCard.TabPane>
