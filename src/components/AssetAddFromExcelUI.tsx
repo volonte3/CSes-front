6f9faf07202132d86fed87ff01c87213fdf0f90a
张航宇 <2021010691@secoder.net>
2023-05-13 16:45:48 +0800

    [007]excel录入功能与后端对接

--- src/components/AssetAddFromExcelUI.tsx
+++ src/components/AssetAddFromExcelUI.tsx
@@ -3,20 +3,86 @@ import {
     UploadOutlined,
     PlusOutlined,
 } from "@ant-design/icons";
-import { Button, Upload, Modal } from "antd";
+import { Button, Upload, Modal, message } from "antd";
 import * as XLSX from "xlsx";
+import { request } from "../utils/network";
+import { LoadSessionID } from "../utils/CookieOperation";
+
+interface ProDic {
+    [key: string]: string;
+}
+
+interface MyDic {
+    [key: string]: string | ProDic;
+}
+
+interface SendDic {
+    [key: string]: MyDic[];
+}
+
+let AddList: MyDic[] = [];
 
 const AssetAddFromExcelUI = () => {
     const [open, setOpen] = useState(false);
     const [confirmLoading, setConfirmLoading] = useState(false);
+    const [itemlist, setitemlist] = useState<any[]>([]);
 
     const handleOk = () => {
         setConfirmLoading(true);
-        setTimeout(() => {
-            setOpen(false);
-            setConfirmLoading(false);
-        }, 2000);
-        
+        for (let i = 0; i < itemlist.length; i++) {
+            const item = itemlist[i];
+            const keys = Object.keys(item);
+            let now_dic: MyDic = {};
+            let pro_dic: ProDic = {};
+            // 遍历所有键并访问键值
+            keys.forEach(key => {
+                const value = item[key];
+                if (key == "资产名称") {
+                    now_dic["Name"] = value;
+                }
+                else if (key == "资产分类") {
+                    now_dic["Type"] = value;
+                }
+                else if (key == "资产数量") {
+                    now_dic["Number"] = value;
+                }
+                else if (key == "资产价值") {
+                    now_dic["Value"] = value;
+                }
+                else if (key == "资产位置") {
+                    now_dic["Position"] = value;
+                }
+                else if (key == "资产描述") {
+                    now_dic["Describe"] = value;
+                }
+                else {
+                    pro_dic[key] = value;
+                }
+            });
+            now_dic["Property"] = pro_dic;
+            AddList.push(now_dic);
+        }
+        request(
+            `/api/Asset/MutiAppend/${LoadSessionID()}`,
+            "POST",
+            {
+                chusheng: AddList,
+            }
+        )
+            .then((res) => {
+                message.success("提交成功");
+                setOpen(false);
+                setConfirmLoading(false);
+                AddList = [];
+            })
+            .catch((err) => {
+                setOpen(false);
+                setConfirmLoading(false);
+                Modal.error({
+                    content: err.message.substring(5),
+                });
+                AddList = [];
+            });
     };
 
     const showModal = () => {
@@ -42,7 +108,7 @@ const AssetAddFromExcelUI = () => {
                     const Sheet = workbook.Sheets["Sheet1"];
                     const Data_json = XLSX.utils.sheet_to_json(Sheet);
                     const Data_list = Array.from(Data_json.values()) as any[];
-                    console.log(Data_list[0]);
+                    setitemlist(Data_list);
                     resolve();
                 } else {
                     reject(new Error("Failed to read file data"));
