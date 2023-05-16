d8f9eee3d0b1af4ef0aad9875fc307a2b235009b
苏禹畅 <suyc21@mails.tsinghua.edu.cn>
2023-05-16 16:00:34 +0800

    [021]资产告警页面#21

---
+++ src/pages/user/asset_manager/asset_warn/index.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { Breadcrumb, Layout, Menu, theme, Space, Table, Tag, Switch, Modal, Button } from "antd";
+const { Column, ColumnGroup } = Table;
+import { useRouter } from "next/router";
+const { Header, Content, Footer, Sider } = Layout;
+import { useState, useEffect } from "react";
+import { request } from "../../../../utils/network";
+import { logout, LoadSessionID } from "../../../../utils/CookieOperation";
+import MemberList from "../../../../components/MemberList";
+import UserInfo from "../../../../components/UserInfoUI";
+import {IfCodeSessionWrong} from "../../../../utils/CookieOperation";
+import SiderMenu from "../../../../components/SiderUI";
+import LogList from "../../../../components/LogListUI";
+import AssetWarnList from "../../../../components/AssetWarnList";
+interface MemberData {
+    Name: string;
+    Department: string;
+    Authority: number;
+    lock: boolean;
+}
+const App = () => {
+    const [state, setState] = useState(true); // 用户是否处在登录状态
+    const [collapsed, setCollapsed] = useState(false);
+    const [UserName, setUserName] = useState<string>(""); // 用户名
+    const [UserAuthority, setUserAuthority] = useState(1); // 用户的角色权限，0超级，1系统，2资产，3员工
+    const [UserApp, setUserApp] = useState<string>(""); // 用户显示的卡片，01串
+    const [Member, setMember] = useState<MemberData[]>(); // 存储加载该系统管理员管理的资产管理员和员工的信息
+    const router = useRouter();
+    const query = router.query;
+    const [Entity, setEntity] = useState<string>(""); // 实体名称
+    const [Department, setDepartment] = useState<string>("");  //用户所属部门，没有则为null
+    const [TOREAD, setTOREAD] = useState(false);
+    const [TODO, setTODO] = useState(false);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+    useEffect(() => {
+        if (!router.isReady) {
+            return;
+        }
+        request(
+            `/api/User/info/${LoadSessionID()}`,
+            "GET"
+        )
+            .then((res) => {
+                setState(true);
+                setUserName(res.UserName);
+                setUserApp(res.UserApp);
+                setUserAuthority(res.Authority);
+                if(res.Authority != 1 ){
+                    Modal.error({
+                        title: "无权访问",
+                        content: "请重新登录",
+                        onOk: () => { window.location.href = "/"; }
+                    });
+                }
+                setEntity(res.Entity);
+                setDepartment(res.Department);
+                setTODO(res.TODO);
+                setTOREAD(res.TOREAD);
+            })
+            .catch((err) => {
+                console.log(err.message);
+                setState(false);
+                Modal.error({
+                    title: "登录失败",
+                    content: "请重新登录",
+                    onOk: () => { window.location.href = "/"; }
+                });
+            });
+    }, [router, query]);
+    if (state) {
+        return (
+            <Layout className="site-layout">
+                <Sider className= "sidebar" width="10%">
+                    <SiderMenu UserAuthority={UserAuthority} />
+                </Sider>
+                <Layout className="site-layout" >
+                    <Header className="ant-layout-header">
+                        <UserInfo Name={UserName} Authority={UserAuthority} Entity={Entity} Department={Department} TODO={TODO} TOREAD={TOREAD}></UserInfo>
+                    </Header>
+                    <Content>
+                        <Breadcrumb style={{ margin: "16px 30px" }}>
+                            <Breadcrumb.Item>资产告警</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
+                            <AssetWarnList/>
+                        </div>
+                    </Content>
+                </Layout>
+            </Layout>
+        );
+    }
+};
+
+export default App;
